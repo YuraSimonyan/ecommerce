5b4fde116fbb0b9c8aa9f5f17cfb6a9f2cf8b0e4
Yura Simonyan <tushe.s.y@gmail.com>
2021-07-11 21:10:06 +0300

    test

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -16,17 +16,17 @@ export class ProductService {
   }
 
   addValueDataBase(value: ProductModel): Observable<any> {
-    return this.http.post<ProductModel>('https://pasasa-fcc65-default-rtdb.firebaseio.com/products.json', value);
+    return this.http.post<ProductModel>('https://database-25cda-default-rtdb.firebaseio.com/products.json', value);
 
   }
 
   editProduct(value, id): void {
-    this.http.patch(`https://pasasa-fcc65-default-rtdb.firebaseio.com/products/${id}.json`, value).subscribe();
+    this.http.patch(`https://database-25cda-default-rtdb.firebaseio.com/products/${id}.json`, value).subscribe();
 
   }
 
   getProduct(): Observable<ProductModel[]> {
-    return this.http.get<ProductModel[]>('https://pasasa-fcc65-default-rtdb.firebaseio.com/products.json');
+    return this.http.get<ProductModel[]>('https://database-25cda-default-rtdb.firebaseio.com/products.json');
 
   }
 
