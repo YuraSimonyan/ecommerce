caa0faa0ffd7a08508e9859fd6a63cc5b785462e
Yura Simonyan <tushe.s.y@gmail.com>
2021-09-15 23:06:44 +0300

    fixed arrows

--- src/app/shell/main/product-detail/product-detail.component.ts
+++ src/app/shell/main/product-detail/product-detail.component.ts
@@ -7,6 +7,7 @@ import {ProductState} from '../../../shared/store/product.state';
 import {MatDialog} from '@angular/material/dialog';
 import {ModalComponent} from '../../../shared/modal/modal.component';
 import {ProductModel} from '../../../shared/models/product.model';
+import {take} from 'rxjs/operators';
 
 @Component({
     selector: 'app-product-detail',
@@ -14,7 +15,7 @@ import {ProductModel} from '../../../shared/models/product.model';
     styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit, OnDestroy {
-    id: string;
+    public id: string;
     @Select(ProductState.productItem)
     public productData$: Observable<ProductModel>;
     public mainPageNum = 0;
@@ -32,35 +33,35 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
 
 
     ngOnInit(): void {
-        this.subscription = this.productData$.subscribe(value => {
-            this.imgLength = value?.img?.length;
-            if (Object.keys(value).length !== 0) {
+        this.subscription = this.productData$.pipe(take(1)).subscribe((product: ProductModel) => {
+            this.imgLength = product?.img?.length;
+            if (Object.keys(product).length !== 0) {
                 this.showPage = true;
             }
         });
-        this.id = this.router.snapshot.params['id'];
+        this.id = this.router.snapshot.params?.id;
         this.store.dispatch(new GetProductsActionById(this.id));
     }
 
-    onChangeImg(event, index): void {
+    public onChangeImg(event, index): void {
         this.mainPageNum = index;
     }
 
-    nextMainImg(): void {
+    public nextMainImg(): void {
         this.mainPageNum++;
         if (this.mainPageNum > this.imgLength - 1) {
             this.mainPageNum = 0;
         }
     }
 
-    backMainImg(): void {
+    public backMainImg(): void {
         this.mainPageNum--;
         if (this.mainPageNum < 0) {
             this.mainPageNum = this.imgLength - 1;
         }
     }
 
-    openDialog(): void {
+    public openDialog(): void {
         this.dialog.open(ModalComponent);
     }
 
